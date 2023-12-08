a127ed932a6e518f9ad55f76699f067c28076081
Eric Minkes <ericminkes@telasoft.nl>
2023-12-08 12:27:57 +0100

    Day 05

--- Helpers.cs
+++ Helpers.cs
@@ -4,4 +4,28 @@
     {
         return c >= '0' && c <= '9';
     }
+
+    public static int BinarySearch<T>(this List<T> list, T item, Func<T, T, int> compare)
+    {
+        return list.BinarySearch(item, new ComparisonComparer<T>(compare));
+    }
+}
+
+public class ComparisonComparer<T> : IComparer<T>
+{
+    private readonly Comparison<T> comparison;
+
+    public ComparisonComparer(Func<T, T, int> compare)
+    {
+        if (compare == null)
+        {
+            throw new ArgumentNullException("comparison");
+        }
+        comparison = new Comparison<T>(compare);
+    }
+
+    public int Compare(T x, T y)
+    {
+        return comparison(x, y);
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
-﻿List<IDay> Days = new()
+﻿using System.Diagnostics;
+
+List<IDay> Days = new()
 {
     new Day01(),
     new Day02(),
     new Day03(),
     new Day04(),
+    new Day05(),
 };
 
 foreach (var day in Days)
 {
-    Console.WriteLine($"Day {day.Number}, part 1:\n{day.Part1()}");
-    Console.WriteLine($"Day {day.Number}, part 2:\n{day.Part2()}\n");
+    var sw = Stopwatch.StartNew();
+    Console.WriteLine($"Day {day.Number}, part 1:\n{day.Part1()}\nTime: {sw.ElapsedMilliseconds}ms");
+    sw.Restart();
+    Console.WriteLine($"Day {day.Number}, part 2:\n{day.Part2()}\nTime: {sw.ElapsedMilliseconds}ms\n");
 }
